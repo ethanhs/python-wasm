72d407ab1528cbe18cb602792c7bea43cb952ef4
Ethan Smith <ethan@ethanhs.me>
2021-11-26 10:39:43 -0800

    Don't disable shared

--- build-python-host-emscripten.sh
+++ build-python-host-emscripten.sh
@@ -5,7 +5,7 @@ export PYTHON_FOR_BUILD="$(pwd)/cpython-build/python"
 cd cpython-host
 
 cp ../config.site-wasm config.site-wasm
-CONFIG_SITE=config.site-wasm READELF=true emconfigure ./configure -C --without-pymalloc --enable-big-digits=30 --with-pydebug --with-suffix=.wasm --with-ensurepip=no --disable-ipv6 --disable-shared --host=wasm32-unknown-emscripten --build=x86_64-unknown-linux-gnu
+CONFIG_SITE=config.site-wasm READELF=true emconfigure ./configure -C --without-pymalloc --enable-big-digits=30 --with-pydebug --with-suffix=.wasm --with-ensurepip=no --disable-ipv6 --host=wasm32-unknown-emscripten --build=x86_64-unknown-linux-gnu
 ln -sfr Modules/Setup.stdlib Modules/Setup.local
 emmake make CROSS_COMPILE=yes FREEZE_MODULE=../cpython-build/Programs/_freeze_module _PYTHON_HOST_PLATFORM=wasm32-unknown-emscripten -j
 emcc -o python.html Programs/python.o libpython3.11d.a -ldl -lm
