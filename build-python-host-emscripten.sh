d97e6ec369da6c430477fa8421044fd10e57629b
Ethan Smith <ethan@ethanhs.me>
2021-11-26 09:48:23 -0800

    Add command to build html for emscripten

--- build-python-host-emscripten.sh
+++ build-python-host-emscripten.sh
@@ -8,3 +8,4 @@ cp ../config.site-wasm config.site-wasm
 CONFIG_SITE=config.site-wasm READELF=true emconfigure ./configure -C --without-pymalloc --enable-big-digits=30 --with-pydebug --with-suffix=.wasm --with-ensurepip=no --disable-ipv6 --disable-shared --host=wasm32-unknown-emscripten --build=x86_64-unknown-linux-gnu
 ln -sfr Modules/Setup.stdlib Modules/Setup.local
 emmake make CROSS_COMPILE=yes FREEZE_MODULE=../cpython-build/Programs/_freeze_module _PYTHON_HOST_PLATFORM=wasm32-unknown-emscripten -j
+emcc -o python.html Programs/python.o libpython3.11d.a -ldl -lm
