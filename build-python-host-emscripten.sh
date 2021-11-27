fa70804757b15497688c791256e3ebbd51662bb8
Ethan Smith <ethan@ethanhs.me>
2021-11-27 06:25:05 -0800

    Use zip for Python stdlib

--- build-python-host-emscripten.sh
+++ build-python-host-emscripten.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
 
+shopt -s extglob
+
 export PYTHON_FOR_BUILD="$(pwd)/cpython/builddir/build/python"
 
 pushd cpython/builddir/host
@@ -21,10 +23,18 @@ rm -rf lib/python3.11/turtledemo
 rm -rf lib/python3.11/wsgiref
 rm -rf lib/python3.11/lib2to3
 rm -rf lib/python3.11/venv
+rm -rf lib/python3.11/idlelib
+rm -rf lib/python3.11/distutils
 rm -rf lib/python3.11/encoding/*.py
 find lib/python3.11 -type f \( -iname \*.opt-1.pyc -o -iname \*.opt-2.pyc \) -delete
+# os.py is a marker for finding the correct lib directory
+# so its important to keep
+cd lib/python3.11
+zip -9 -r ../python311.zip * -x lib/python3.11/os.py
+rm -vrf !(os.py|encoding/__pycache__)
+cd ../..
 mkdir -p lib/python3.11/lib-dynload
-touch lib/python3.11/lib-dynload
+touch lib/python3.11/lib-dynload/.gitignore
 popd
-emcc -Os -o python.html Programs/python.o libpython3.11d.a Modules/_decimal/libmpdec/libmpdec.a Modules/expat/libexpat.a -ldl -lm -s LZ4=1 --preload-file ../usr
+emcc -Os -o python.html Programs/python.o libpython3.11d.a Modules/_decimal/libmpdec/libmpdec.a Modules/expat/libexpat.a -ldl -lm -s USE_ZLIB --preload-file ../usr
 popd
