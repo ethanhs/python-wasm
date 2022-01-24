13fb486adafd47b606dcf4b993667aaa6163ef3f
Christian Heimes <christian@python.org>
2022-01-24 22:26:16 +0100

    Use /usr/bin/env
    Fixes: https://github.com/ethanhs/python-wasm/issues/52
    Signed-off-by: Christian Heimes <christian@python.org>

--- run-python-browser.sh
+++ run-python-browser.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 cp cpython/builddir/emscripten-browser/python.* browser-ui/worker/
 
--- run-python-node.sh
+++ run-python-node.sh
@@ -1,3 +1,3 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 node --experimental-wasm-threads --experimental-wasm-bulk-memory cpython/builddir/emscripten-node/python.js $@
