e12594660a023ff8c9e4c9463115ceb1f4ad18e8
Christian Heimes <christian@python.org>
2022-01-25 17:24:12 +0100

    Add test runner script

---
+++ test-emscripten-node.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+if [ $# -eq 0 ]; then
+    ARGS="-w -f node-tests.txt"
+else
+    ARGS="$@"
+fi
+
+node --experimental-wasm-threads --experimental-wasm-bulk-memory \
+    cpython/builddir/emscripten-node/python.js \
+    -m test $ARGS
