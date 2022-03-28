cef91af7ded9ff456eec633f346e085a9ccb038a
Christian Heimes <christian@python.org>
2022-03-28 20:33:08 +0300

    Pass args to configure, run all tests (#67)

--- test-emscripten-node.sh
+++ test-emscripten-node.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-if [ $# -eq 0 ]; then
-    ARGS="-w -f node-tests.txt"
-else
-    ARGS="$@"
-fi
-
 exec node --experimental-wasm-threads --experimental-wasm-bulk-memory \
     cpython/builddir/emscripten-node/python.js \
-    -m test $ARGS
+    -m test "$@"
