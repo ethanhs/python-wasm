89a73b67b16fa7baca59ec56cee446eacc341efd
Christian Heimes <christian@python.org>
2022-06-02 09:38:48 +0200

    Move link hack into build dir

--- run-python-wasi.sh
+++ run-python-wasi.sh
@@ -4,6 +4,4 @@ set -e
 export PATH="$PATH:/root/.wasmtime/bin"
 
 cd cpython/builddir/wasi
-# XXX hack, symlink sysconfig data into stdlib directory
-ln -srf -t ../../Lib/ build/lib.wasi-wasm32-3.*/_sysconfigdata__wasi_wasm32-wasi.py
 exec wasmtime run --mapdir .::../../ -- python.wasm "$@"
